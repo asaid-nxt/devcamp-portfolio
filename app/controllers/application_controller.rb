4204c12512ca91ac4da98555a064fcb4ef3b833f
asaid-nxt <asaid@nxt.so>
2024-07-29 19:44:34 +0300

    added name attribute for registration and edit views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_pararmeters, if: :devise_controller?
+
+  def configure_permitted_pararmeters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end
