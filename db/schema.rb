6994582259d586de9a5c63cab0de0b3762c29157
asaid-nxt <asaid@nxt.so>
2024-08-06 13:06:53 +0300

    updated portfolio to include position attr

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_01_123837) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_06_095318) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,6 +45,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_01_123837) do
     t.text "thumb_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "position"
   end
 
   create_table "skills", force: :cascade do |t|
