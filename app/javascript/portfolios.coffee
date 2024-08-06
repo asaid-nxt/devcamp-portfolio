4f3fd4f287055a1abd9d36c0aae56aa50e77bb88
asaid-nxt <asaid@nxt.so>
2024-08-06 16:48:31 +0300

    integrated initail drag and drop implemented

---
+++ app/javascript/portfolios.coffee
@@ -0,0 +1,8 @@
+# app/assets/javascripts/custom/sortable.coffee
+
+ready = ->
+  console.log("Document is ready")
+  $('.sortable').sortable()
+  return
+
+$(document).ready ready
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,11 +1,22 @@
-# Pin npm packages by running ./bin/importmap
+pin "application", preload: true
 
-pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+# Pin jQuery and jQuery UI
+pin "jquery", to: "https://code.jquery.com/jquery-3.6.0.min.js", preload: true
+pin "jquery-ui", to: "https://code.jquery.com/ui/1.12.1/jquery-ui.min.js", preload: true
+pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.0/lib/assets/compiled/rails-ujs.js", preload: true
+pin "portfolios", to: "portfolios.js"
+
+
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.0/lib/assets/compiled/rails-ujs.js"
-pin "bootstrap", to: "bootstrap.min.js", preload: true
-pin "@popperjs/core", to: "popper.js", preload: true
-pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.3/dist/js/bootstrap.esm.js"
+
+
+# Pin Popper.js
+pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.6/dist/esm/popper.js", preload: true
+
+# Pin Bootstrap
+pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.3/dist/js/bootstrap.esm.js", preload: true
+
+# No direct equivalent for require_tree in Importmaps; import files manually
