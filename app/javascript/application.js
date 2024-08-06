3072d55f8e447396d456cc937afd516aa20ab748
asaid-nxt <asaid@nxt.so>
2024-08-06 13:48:43 +0300

    installed jquery ui and html5 sortable

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,7 +3,9 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "bootstrap"
 import Rails from "@rails/ujs"
+import "./html.sortable"
 Rails.start()
 //= require jquery3
+//= require jquery-ui
 //= require popper
 //= require bootstrap-sprockets
